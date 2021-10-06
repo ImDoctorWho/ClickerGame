9c965f78e8454dc32e0f91c05face736fe975da3
ImDoctorWho <48880085+ImDoctorWho@users.noreply.github.com>
2021-10-06 15:43:14 +0500

    Create menu
    -Добавлено меню
    -Добавлен лес

---
+++ 2D Clicker/Assets/Scripts/InizializedGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InizializedGame : MonoBehaviour
+{
+    public void Awake()
+    {
+       
+    }
+}
---
+++ 2D Clicker/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    
+
+    public void ClickMaterialsButton()
+    {
+        
+    }
+}
